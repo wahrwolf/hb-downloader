d08f6193077b19afc3d0d57c8ed63e9dfb0c0e30
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-02-09 00:21:48 +0100

    easy_install: adds minimal setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hb_downloader',
+      version='0.5.0',
+      description='an unofficial api client for humblebundle',
+      url='https://github.com/MayeulC/hb-downloader/releases',
+      author='Brian Schkerke',
+      license='MIT',
+      packages=['hb_downloader'],
+      zip_safe=False)
