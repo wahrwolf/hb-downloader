6514ae30bbaba09841ab3c17ef2fed7a293ad896
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-02-09 02:14:02 +0100

    easy_install: includes subpackage

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ setup(name='hb_downloader',
       url='https://github.com/MayeulC/hb-downloader/releases',
       author='Brian Schkerke',
       license='MIT',
-      packages=['hb_downloader'],
+      packages=[
+          'hb_downloader',
+          'hb_downloader.humble_api',
+          'hb_downloader.humble_api.model',
+          'hb_downloader.humble_api.exceptions'
+          ],
       test_suite='nose.collector',
       tests_require=['nose'],
       zip_safe=False)
